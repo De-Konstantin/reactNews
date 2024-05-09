72e3d6e86dc8b1f59187690aacceea284e26fa19
De-Konstantin <denkakonstantinov@gmail.com>
2024-05-09 12:09:41 +0300

    rout

---
+++ src/app/appRouter.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router-dom';
+import BaseLayout from './layouts/BaseLayout';
+import { MainPage } from '@/pages/main';
+
+export const appRouter = createBrowserRouter([
+  {
+    element: <BaseLayout />,
+    errorElement: <div>Error</div>,
+    children: [
+      {
+        path: '/',
+        element: <MainPage />,
+      },
+      {
+        path: '/news/:id',
+        element: <div>id</div>,
+      },
+    ],
+  },
+]);
---
+++ src/pages/news/ui/Page.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css';
+
+const NewsPage = () => {
+  return <main className={styles.news}></main>;
+};
+
+export default NewsPage;
