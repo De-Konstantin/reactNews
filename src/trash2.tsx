f3639adbacff48c904de012395cc8aa9c6c85490
De-Konstantin <denkakonstantinov@gmail.com>
2024-06-09 21:02:22 +0300

    tr-13

--- src/trash2.tsx
+++ src/trash2.tsx
@@ -85,6 +85,12 @@ const Categories = forwardRef(
             className={!selectedCategory ? styles.active : styles.item}
             type="button">
             All
+          </button>
+          <button
+            onClick={() => setSelectedCategory(null)}
+            className={!selectedCategory ? styles.active : styles.item}
+            type="button">
+            All
           </button>{' '}
           <button
             onClick={() => setSelectedCategory(null)}
